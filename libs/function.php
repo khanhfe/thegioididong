4465c6d0bedb4bcd2d65f8488c14aa45ad93d486
Khanh <cajjco123@gmail.com>
2020-07-15 13:26:26 +0700

    update admin ui

--- libs/function.php
+++ libs/function.php
@@ -93,5 +93,19 @@
 		$query = mysqli_query($conn, $sql);
 		return $query;
 	}
+
+	function info_orders(){
+		global $conn;
+		connect_db();
+		$sql = "SELECT * FROM customer JOIN orders ON customer.CustomID = orders.CustomID";
+		$query = mysqli_query($conn, $sql);
+		$result = array();
+		if($query) {
+			while($row = mysqli_fetch_assoc($query)){
+				$result[] = $row;
+			}
+		}
+		return $result;
+	}
 	
 ?>
--- login/admin/dist/tables.php
+++ login/admin/dist/tables.php
@@ -5,22 +5,21 @@
     disconnect_db();
 ?>
 <!DOCTYPE html>
-<html lang="vi">
+<html lang="en">
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Thegioididong Admin</title>
-        <link href="../../../img/icon/favicon.ico" rel="shortcut icon" type="image/x-icon">
+        <title>Tables - SB Admin</title>
         <link href="css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.php">Welcome</a>
+            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
@@ -39,7 +38,7 @@
                         <a class="dropdown-item" href="#">Settings</a>
                         <a class="dropdown-item" href="#">Activity Log</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="../../index.php">Logout</a>
+                        <a class="dropdown-item" href="login.html">Logout</a>
                     </div>
                 </li>
             </ul>
@@ -50,7 +49,7 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="index.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
@@ -110,25 +109,32 @@
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small">Logged in as:</div>
-                        Khảnh Nguyễn
+                        Start Bootstrap
                     </div>
                 </nav>
             </div>
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid">
-                        <h1 class="mt-4">Dashboard</h1>
+                        <h1 class="mt-4">Tables</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Dashboard</li>
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Tables</li>
                         </ol>
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
+                                <a target="_blank" href="https://datatables.net/">official DataTables documentation</a>
+                                .
+                            </div>
+                        </div>
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table mr-1"></i>
-                                List Product
+                                DataTable Example
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
-                                    <a href="../add-product.php" class="add">Thêm mới sản phẩm</a>
                                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                         <thead>
                                             <tr>
@@ -173,85 +179,7 @@
                                                     <a href="../delete-product.php?id=<?php echo $item['ProductId'];?>" onclick="return confirm('Xóa sản phẩm ?')" class="del">Delete</a>
                                                 </td>
                                             </tr>
-                                                <?php } ?>  
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-area mr-1"></i>
-                                        Area Chart Example
-                                    </div>
-                                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-bar mr-1"></i>
-                                        Bar Chart Example
-                                    </div>
-                                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table mr-1"></i>
-                                List Orders
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                        <thead>
-                                            <tr>
-                                                <th>ID</th>
-                                                <th>Name Product</th>
-                                                <th>Image Product</th>
-                                                <th>Promotion Price (if available)</th>
-                                                <th>Original Price </th>
-                                                <th>Color</th>
-                                                <th>Quantity</th>
-                                                <th>Total Pay</th>
-                                                <th>Order Date</th>
-                                                <th>Estimated Delivery Time</th>
-                                            </tr>
-                                        </thead>
-                                        <tfoot>
-                                            <tr>
-                                                <th>ID</th>
-                                                <th>Name Product</th>
-                                                <th>Image Product</th>
-                                                <th>Promotion Price (if available)</th>
-                                                <th>Original Price </th>
-                                                <th>Color</th>
-                                                <th>Quantity</th>
-                                                <th>Total Pay</th>
-                                                <th>Order Date</th>
-                                                <th>Estimated Delivery Time</th>
-                                            </tr>
-                                        </tfoot>
-                                        <tbody>
-                                            <tr>
-                                                <?php
-                                                    foreach ($orders as $data) {?>
-                                                <td><?php echo $data['OrderID']; ?></td>
-                                                <td class="name"><?php echo $data['Product']; ?></td>
-                                                <td><img src="<?php echo '../../../'.$data['Image']; ?>" width="100px"></td>
-                                                <td class="price"><?php echo number_format($data['PricePromote']); ?>đ</td>
-                                                <td class="price old"><?php echo number_format($data['PriceUnit']); ?>đ</td>
-                                                <td class="brand"><?php echo $data['Color']; ?></td>
-                                                <td><?php echo $data['Quantity']; ?></td>
-                                                <td class="group"><?php echo number_format($data['TotalPay']); ?>đ</td>
-                                                <td><?php echo $data['OrderDate'];?></td>
-                                                <td><?php echo $data['EstimatedDeliveryTime'];?></td>
-                                            </tr>
                                                 <?php } ?>
-                                            <td valign="top" colspan="6" class="dataTables_empty">No data available in table</td> 
                                         </tbody>
                                     </table>
                                 </div>
@@ -262,7 +190,7 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; thegioididong 2020</div>
+                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
@@ -276,9 +204,6 @@
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
         <script src="assets/demo/datatables-demo.js"></script>
